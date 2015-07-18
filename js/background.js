c96da7fdef625d9e54de385e791fc8b92a131664
Brad Weikel <brad@ThunderBeard.local>
2015-07-18 08:59:44 -0600

    Fixed bootstrap inclusion bug, and some cleanup

--- js/background.js
+++ js/background.js
@@ -4,6 +4,9 @@ chrome.webRequest.onBeforeRequest.addListener(
   function(info) { 
     return {redirectUrl: chrome.extension.getURL("/uTurn.html") };
   },
-  {urls: url_list}, 
+  {
+    urls: url_list, 
+    types: ['main_frame'] 
+  }, 
   ["blocking"]
 );
