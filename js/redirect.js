11e85735c55482f6326957e09d3493eb10c11c2d
Brad Weikel <bradweikel@users.noreply.github.com>
2015-07-29 01:19:26 -0600

    basic icon randomizing functionality

--- js/redirect.js
+++ js/redirect.js
@@ -1,4 +1,42 @@
+var Route = function (image, description, url) {
+  this.image = image;
+  this.description = description;
+  this.url = url;
+}
 
+var routes = [
+  [
+    new Route ("/resources/images/dishes.jpg", "Wash a few dishes.", ""),
+    new Route ("/resources/images/cleandesk.jpg", "Straighten up your desk.", ""),
+    new Route ("/resources/images/inboxzero.jpg", "Clear out some email. Can you reach Inbox Zero?", ""),
+  ],[
+    new Route ("/resources/images/water.jpg", "Drink a glass of water.", ""),
+  ],[
+    new Route ("/resources/images/rice.jpg", "Make rice", ""),
+  ],[
+    new Route ("/resources/images/npr.jpg", "Great storytelling and rigorous reporting.", "http://npr.org"),
+  ],
+];
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 
 var bRunning = true;
@@ -16,6 +54,16 @@ window.addEventListener('blur', function() {
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
+  shuffle(routes[0]);
+  shuffle(routes[1]);
+  shuffle(routes[2]);
+  shuffle(routes[3]);
+
+  var icons = ["#icon1","#icon2","#icon3","#icon4"];
+
+  for (i = 0; i < icons.length; i++) { 
+    $(icons[i]).html("<img src=" + routes[i][0].image + "/>");
+  }
 
   setInterval(function () {
     if(bRunning && pane == 3) {
